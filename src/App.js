24d04d852016e7aa1129e166c3e25445eea7ce11
Rittik Das <dassrittik1998@gmail.com>
2023-07-04 23:10:08 +0530

    refractor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ToastWrapper from "./component/toast/toastWrapper";
 import AppRouter from "./routers/AppRouter";
 
 function App() {
   return (
     <div className="App">
+      <ToastWrapper />
       <AppRouter />
     </div>
   );
