010dada5feef584714e3bd3f6861b1e29c0df5ae
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 22:45:00 +0530

    Update

---
+++ src/services/userServices.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export const getAllUsersServices = async () => await axios.get("/api/users");
+
+export const getUserByIdService = async (userId) =>
+  await axios.get(`/api/users/${userId}`);
+
+export const addToBookmarkService = async (postId, encodedToken) =>
+  await axios.post(
+    `/api/users/bookmark/${postId}`,
+    {},
+    {
+      headers: { authorization: encodedToken },
+    }
+  );
+
+export const removePostFromBookmarkService = async (postId, encodedToken) =>
+  await axios.post(
+    `api/users/remove-bookmark/${postId}`,
+    {},
+    { headers: { authorization: encodedToken } }
+  );
+
+export const getBookmarkService = async (encodedToken) =>
+  await axios.get("/api/users/bookmark", {
+    headers: { authorization: encodedToken },
+  });
+
+export const followUserService = async (followUserId, encodedToken) =>
+  await axios.post(
+    `/api/users/follow/${followUserId}`,
+    {},
+    {
+      headers: { authorization: encodedToken },
+    }
+  );
+
+export const unfollowUserService = async (unfollowUserId, encodedToken) =>
+  await axios.post(
+    `/api/users/unfollow/${unfollowUserId}`,
+    {},
+    { headers: { authorization: encodedToken } }
+  );
+
+export const editUserService = async (dataInput, encodedToken) => {
+  console.log(dataInput);
+
+  return await axios.post(
+    "/api/users/edit",
+    { userData: dataInput },
+    { headers: { authorization: encodedToken } }
+  );
+};
---
+++ src/utils/constant.js
@@ -0,0 +1,29 @@
+export const dummyData = {
+  username: "rittikdas",
+  password: "abcde",
+};
+
+export const actionTypes = {
+  GET_ALL_USER: "GET_ALL_USER",
+  GET_ALL_POST: "GET_ALL_POST",
+  GET_USER_SEARCH: "GET_USER_SEARCH",
+  GET_BOOKMARK: "GET_BOOKMARK",
+  FOLLOW_USER: "FOLLOW_USER",
+  LIKE_POST: "LIKE_POST",
+  DISLIKE_POST: " DISLIKE_POST",
+  CREATE_POST: "CREATE_POST",
+  DELETE_POST: "DELETE_POST",
+  UPDATE_POST: "UPDATE_POST",
+  EDIT_POST: "EDIT_POST",
+  UPDATE_USER: "UPDATE_USER",
+  UPDATE_USER_PROFILE: "UPDATE_USER_PROFILE",
+};
+
+export const avatarImages = [
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688726507/Avatar_1_t9nafi.jpg",
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688726507/Avatar_2_wmpyni.jpg",
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688726507/Avatar_5_ifgfxr.jpg",
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688744813/Avatar_7_vgeebv.jpg",
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688726507/Avatar_6_sjdadv.jpg",
+  "https://res.cloudinary.com/dqz5b6jq9/image/upload/v1688726507/Avatar_3_ruuadp.jpg",
+];
