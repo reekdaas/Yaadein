a65f3812e13a7deb34b831108fd0d252bd3f2e44
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 22:44:19 +0530

    Update

--- src/pages/likedProducts/likedProducts.jsx
+++ src/pages/likedProducts/likedProducts.jsx
@@ -1,5 +1,5 @@
 import styles from "./likedProducts.module.css";
 
-export default function LikedProductsPage() {
+export default function LikedPostsPage() {
   return <div>This is Liked Products page</div>;
 }
