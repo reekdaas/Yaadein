f3d456adc7af7b4c8e8b83f1bea592f27ab00bb4
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 22:40:30 +0530

    Update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,96 @@
 import { createContext, useContext, useState } from "react";
+import { logInService, signUpService } from "../services/authService";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router";
 
 const AuthContext = createContext(null);
 export function AuthContextProvider({ children }) {
+  const navigate = useNavigate();
   const [token, setToken] = useState(localStorage?.getItem("token"));
   const [userData, setUserData] = useState(
-    JSON.parse(localStorage?.getItem("userDetails"))
+    JSON.parse(localStorage?.getItem("userDetails")) || null
   );
-  console.log(token);
-  console.log(userData);
-  const value = { token, userData, setToken, setUserData };
+  const [loading, setLoading] = useState(false);
+
+  const userLogIn = async (userData) => {
+    try {
+      setLoading(true);
+      console.log(userData);
+      const response = await logInService(userData);
+      console.log(response);
+      const {
+        data: { encodedToken: token, foundUser },
+      } = response;
+      console.log(response);
+      localStorage.setItem("token", token);
+      localStorage.setItem("userDetails", JSON.stringify(foundUser));
+      setToken(token);
+      setUserData(foundUser);
+      toast.success("Welcome Back");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      toast.error("Something Went Wrong,Check Your Input!");
+    } finally {
+      setLoading(false);
+    }
+  };
+  const userLogOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userDetails");
+    setToken(null);
+    setUserData(null);
+    toast.success("Logged Out Successfully!!");
+    navigate("/login");
+  };
+
+  const signUpUser = async ({
+    firstName,
+    lastName,
+    username,
+    password,
+    avatarUrl,
+  }) => {
+    try {
+      setLoading(true);
+      const response = await signUpService(
+        firstName.trim(),
+        lastName.trim(),
+        username.trim(),
+        password.trim(),
+        avatarUrl
+      );
+      const {
+        data: { createdUser, encodedToken },
+        status,
+      } = response;
+      if (status === 201) {
+        setUserData(createdUser);
+        setToken(encodedToken);
+        toast.success("Welcome User");
+        navigate("/");
+      }
+    } catch (err) {
+      const { status } = err;
+      if (status === 422) {
+        toast.error("Username already exist!");
+      } else toast.error("Something went wrong");
+      // console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    token,
+    userData,
+    setToken,
+    setUserData,
+    signUpUser,
+    loading,
+    userLogIn,
+    userLogOut,
+  };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,15 @@
+import {
+  UserContextProvider,
+  useAllUserContext,
+} from "./userContext/allUserContext";
+import {
+  usePostContext,
+  PostContextProvider,
+} from "./postContext/allPostsContext";
+
+export {
+  UserContextProvider,
+  useAllUserContext,
+  usePostContext,
+  PostContextProvider,
+};
