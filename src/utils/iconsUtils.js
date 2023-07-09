b4885d783247a8912abae67a24eb6e931eeb1d71
Rittik Das <dassrittik1998@gmail.com>
2023-07-09 22:45:11 +0530

    Update

---
+++ src/utils/iconsUtils.js
@@ -0,0 +1,7 @@
+import { AiFillHome } from "react-icons/ai";
+import { MdOutlineExplore } from "react-icons/md";
+import { BiBookmarks } from "react-icons/bi";
+import { BsPersonCircle } from "react-icons/bs";
+import { FiLogOut } from "react-icons/fi";
+
+export { AiFillHome, MdOutlineExplore, BiBookmarks, BsPersonCircle, FiLogOut };
